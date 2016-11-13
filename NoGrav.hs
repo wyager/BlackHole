a548df9e47aac3872bacd414069ae8bf45b8032a
Will Yager <will.yager@gmail.com>
2016-11-12 19:32:51 -0600

    Switched to vectors

--- NoGrav.hs
+++ NoGrav.hs
@@ -7,18 +7,18 @@ import qualified Codec.Picture.Png as Png
 import qualified PerlinG as Perlin
 import qualified XYZ
 import qualified XY
-import Data.Array.Repa ((:.)((:.)), (!), Z(Z))
-import qualified Data.Array.Repa as Repa
-import qualified Data.Array.Repa.Eval as Eval
-import Data.Array.Repa.Repr.Vector as Vec
+import qualified Data.Vector as Vec
 import qualified Control.Monad.Identity as Identity
 import qualified Data.Colour.RGBSpace as RGB
 import qualified Data.Colour.RGBSpace.HSV as HSV
+import qualified Data.Vector.Strategies as Strat
 import Data.Word (Word64)
 
 data Collision = Accretion Point
                | BlackHole Point
                | Celestial Point
+
+type Color = V3 Double
 
 data Config = Config {
     scale :: Double,
@@ -27,18 +27,19 @@ data Config = Config {
     celestialRadius :: Double
 }
 
-trace :: Config -> Point -> Direction -> (Collision, Int)
-trace (Config scale bhr ar cr) start direction = go start 0 
+trace :: Config -> Point -> Direction -> (Color, Int)
+trace cfg@(Config scale bhr ar cr) start direction = go 1 (V3 0 0 0) start 0 
     where
     direction' = unit direction 
-    go start n
-        | Just isect <- accretion = (Accretion isect, n)
-        | Just isect <- blackhole = (BlackHole isect, n)
-        | celestial = (Celestial end, n)
-        | otherwise = go end (n+1)
+    go opacity color start n
+        | opacity <= 0.01 = (color, n)
+        | Just isect <- accretionI = go (opacity * 0.6) (color <+> accretionColor cfg isect) end (n+1)
+        | Just isect <- blackholeI = (color <+> blackholeColor isect, n)
+        | celestial = (color <+> celestialColor end, n)
+        | otherwise = go opacity color end (n+1)
         where
         -- Finds the point in the middle that hits the disk (y = 0)
-        accretion 
+        accretionI 
             | (y start > 0) == (y end > 0) = Nothing -- Do we cross the disk plane?
             | crossSquared < (ar * ar) && crossSquared > (bhr * bhr) = Just accretionCross -- Is the crossing within the disk's radius?
             | otherwise = Nothing
@@ -53,7 +54,8 @@ trace (Config scale bhr ar cr) start direction = go start 0
                 distance = y_start - y_end
                 c_start = abs (y_end / distance)
                 c_end = 1 - c_start
-        blackhole
+        -- Does the ray hit the black hole?
+        blackholeI
             | startDistance > (bhr + adjustedScale) = Nothing -- No way Jose
             | (start <.> start) < (bhr * bhr) = Just start -- We inside 
             | closestSquared < (bhr * bhr) = Just start -- Inside at some point
@@ -85,40 +87,34 @@ point2xyz pt = XYZ.XYZ x y z
 noiseWith :: Int -> Point -> Double
 noiseWith seed = Perlin.perlin (XYZ.noise seed) (+) (*) XYZ.weight 4 . point2xyz
 
-stars :: Point -> Pic.PixelRGB8
-stars pt = Pic.PixelRGB8 pixelVal pixelVal pixelVal
+celestialColor :: Point -> Color
+celestialColor pt = V3 brightness brightness brightness
     where 
-    pixelVal = round $ 255 * brightness
     brightness = if noise < 0.70 then 0 else sigmoid noise
     seed = 0x1337
     noise = noiseWith seed $ fmap (/1e2) pt
     sigmoid v = 1 / (1 + exp (negate 30 * (v - 0.8)))
 
-accretion :: Config -> Point -> Pic.PixelRGB8
-accretion config pt@(V3 x y z) = RGB.uncurryRGB Pic.PixelRGB8 rgb
+accretionColor :: Config -> Point -> Color
+accretionColor config pt@(V3 x y z) = RGB.uncurryRGB V3 rgb
     where
     l = (lengthOf pt - blackHoleRadius config) / (accretionRadius config - blackHoleRadius config)
-    rgb = fmap (round . (*255)) $ HSV.hsv (30-l*30) 1 (0.8 + oscillation)
+    rgb = HSV.hsv (30-l*30) 1 (0.8 + oscillation)
     noiseY = noiseWith 0xBEEF $ fmap (/1e2) pt
     noiseX = noiseWith 0xCAFE $ fmap (/1e2) pt
     scrambled = V3 (x + noiseX*1000) (y + noiseY*1000) z
     oscillation = (0.2*) $ sin $ (/400) $ lengthOf scrambled
 
-blackhole :: Point -> Pic.PixelRGB8
-blackhole pt@(V3 x y z) = Pic.PixelRGB8 b b b
+blackholeColor :: Point -> Color
+blackholeColor pt@(V3 x y z) = V3 b b b
     where
     phi   = atan2 x z
     theta = atan2 x y
-    b = round $ (*255) $ (/2) $ (1+) $ sin $ (/1e3) $ x + y + z
+    b = (/2) $ (1+) $ sin $ (/1e3) $ x + y + z
 
-ray :: Double -> Double -> (Pic.PixelRGB8, Int)
-ray x y = (pixel, count)
+ray :: Double -> Double -> (Color, Int)
+ray x y = (color, count)
     where
-    count = snd coll
-    pixel = case fst coll of
-        Accretion pt -> accretion config pt
-        BlackHole pt -> blackhole pt
-        Celestial pt -> stars pt
     config = Config {
             scale           = 1  * 1e3,
             blackHoleRadius = 10 * 1e3,
@@ -132,37 +128,37 @@ ray x y = (pixel, count)
     up :: Point
     up = (direction <%> right)
     screenPoint = direction <+> (scaleBy x right) <+> (scaleBy y up)
-    coll = trace config camera screenPoint
+    (color, count) = trace config camera screenPoint
 
-pixel :: Int -> Int -> Int -> Int -> (Int, Pic.PixelRGB8)
+pixel :: Int -> Int -> Int -> Int -> (Int, Color)
 pixel w h x y = (count, pixel)
     where
     (pixel, count) = ray (xf * fov) (yf * fov)
-    fov = 1 / 12
+    fov = 1/20 -- 1 / 12
     [h', w', x', y'] = map (\l -> fromIntegral l) [h,w,x,y]
     xf = (x' - w'/2) / h' -- We actually want these to be the same to avoid stretching
     yf = (y' - h'/2) / h'
 
-pixels :: Int -> Int -> (Int, Repa.Array Vec.V (Z :. Int :. Int) Pic.PixelRGB8)
-pixels w h = Identity.runIdentity $ do
-    output <- Vec.computeVectorP image
-    count  <- Repa.foldAllP (+) 0 $ Repa.map fst output 
-    pixels <- Vec.computeVectorP $ Repa.map snd output
-    return $! (count, pixels)
+pixels :: Int -> Int -> (Int, Vec.Vector (Vec.Vector Pic.PixelRGB8))
+pixels w h = (count, pixels)
     where
-    image = Repa.fromFunction (Z :. w :. h) 
-          $ \(Z :. x :. y) -> pixel w h x y
-    -- result :: Repa.Array V (Z :. Int :. Int) (Int, Pic.PixelRGB8)
-    -- result = Identity.runIdentity $ Repa.computeP image
-    -- pixels = Identity.runIdentity $ Repa.computeP $  -- result
-    -- steps = 0 -- Identity.runIdentity $ Repa.foldAllP (+) 0 $ Repa.map fst result
-
+    map2 f = Vec.map (Vec.map f)
+    results = Vec.generate w $ \x -> Vec.generate h $ \y -> pixel w h x y
+    counts = map2 fst results
+    count = Vec.sum (Vec.map Vec.sum counts)
+    colors = map2 snd results
+    amplitude (V3 r g b) = max r (max g b)
+    amplitudes = map2 amplitude colors 
+    maxAmplitude = Vec.maximum (Vec.map Vec.maximum amplitudes)
+    scaled = map2 (fmap (/maxAmplitude)) colors
+    word8s = map2 (fmap (round . (*255))) scaled
+    pixels = map2 (\(V3 r g b) -> Pic.PixelRGB8 r g b) word8s
 
 
 w = 400
 h = 200
 (steps, array) = pixels w h
-image = Pic.generateImage (\x y -> array ! (Z :. x :. y)) w h
+image = Pic.generateImage (\x y -> (array Vec.! x) Vec.! y) w h
 
 
 main = do
