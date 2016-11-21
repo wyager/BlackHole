6e21fea30a1f6572f3f763ab33bc261a07b30ba3
Will Yager <will.yager@gmail.com>
2016-11-20 21:53:07 -0800

    working on spiral

--- Grav.hs
+++ Grav.hs
@@ -158,7 +158,7 @@ accretionLight config pt@(V3 x y z) len = scaleLight (farFalloff * nearFalloff *
     noise = 0 -- (4*) $ noiseWith 0xCAFE $ fmap (200*) pt
     -- scrambled = V3 (x + noiseX*1000) (y + noiseY*1000) z
     r = lengthOf pt
-    oscillation = (0.4*) $ (^3) $ sin $ (+ noise) $ ((50*r)+) $ (2*pi*) $ atan2 x z
+    oscillation = (0.4*) $ (^3) $ sin $ (+ noise) $ ((25*r)+) $ atan2 x z
     width = (oscillation + 0.6) * accretionWidth config
     -- oscillation = (0.2*) $ sin $ (/400) $ lengthOf scrambled
 
