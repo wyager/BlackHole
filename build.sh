342f89a1ce068b0cf473436aa04eb6b074133614
Will Yager <will.yager@gmail.com>
2016-11-29 16:39:21 -0600

    Add build stuff

---
+++ build.sh
@@ -0,0 +1,6 @@
+export CABAL=/lusr/opt/ghc-7.10.3/bin/cabal
+echo "Installing dependencies (may take a few minutes)"
+$CABAL sandbox init
+$CABAL install --only-dependencies -j8
+echo "Building executable"
+$CABAL build
