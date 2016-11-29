397aa6bba672415321e26db3608f0cf90482af89
Will Yager <will.yager@gmail.com>
2016-11-29 16:29:57 -0600

    Deleted stuff

--- Polar.hs
+++
@@ -1,9 +0,0 @@
--- NB: Borrowed from my own project, GeoLabel
-module Polar (Polar(..)) where
-
-import Prelude () -- Don't import anything from standard prelude
-import Numeric.Units.Dimensional.Prelude
-
-data Polar = Polar { r :: Length Double, 
-                     theta :: Angle Double, 
-                     phi :: Angle Double } deriving Show
